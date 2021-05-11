175357cca058015bee222a2e08c7e290c25ab5b6
Alex808r <fromxlam@gmail.com>
2021-05-11 20:13:57 +0300

    cart add quantity

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,6 +25,7 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
+
     @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
