9a1ecbb9116ac342a63e241ac0b013117c1e2277
Alex808r <fromxlam@gmail.com>
2021-05-21 12:50:59 +0300

    chapter 11 add AJAX

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order(:title) # order(:title) - сортировка в алфавитном порядке по наименованию.
                                       # (:title - название столбца из базы даннных)
