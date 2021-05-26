d6afdd423570b6ca1ded054464a17722a45a6ac6
Alex808r <fromxlam@gmail.com>
2021-05-26 17:37:57 +0300

    minor change

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
 
-  validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\z}i,
+  validates :image_url, allow_blank: true,
+    format:  {with: %r{\.(gif|jpg|png)\z}i,
     message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
   }
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -16,12 +16,13 @@ class ProductTest < ActiveSupport::TestCase
     test "product price must be positive" do
       # цена товара должна быть положительной
       product = Product.new(title: "My Book Title",
-                            description: "yyy",
-                            image_url: "zzz.jpg")
+                                    description: "yyy",
+                                    image_url: "zzz.jpg")
       product.price = -1
       assert product.invalid?
       assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
                         # должна быть больше или равна 0.01
+
       product.price = 0
       assert product.invalid?
       assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
@@ -41,15 +42,14 @@ class ProductTest < ActiveSupport::TestCase
       ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
   http://a.b.c/x/y/z/fred.gif }
       bad = %w{ fred.doc fred.gif/more fred.gif.more }
-      ok.each do |image_url|
-        assert new_product(image_url).valid?,
-               "#{image_url} shouldn't be invalid"
-                         # не должно быть неприемлемым
+      ok.each do |name|
+        assert new_product(name).valid?,"#{name} shouldn't be invalid"
+                                                         # не должно быть неприемлемым
       end
+
       bad.each do |image_url|
-        assert new_product(image_url).invalid?,
-               "#{image_url} shouldn't be valid"
-                        # не должно быть приемлемым
+        assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+                                                         # не должно быть приемлемым
       end
     end
 
