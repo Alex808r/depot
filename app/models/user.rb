5f108edb8120a71cff3e4922315e874c209bfba3
Alex808r <fromxlam@gmail.com>
2021-06-02 11:18:35 +0300

    add validate model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  #Мы проверяем наличие и уникальность имени (то есть в базе данных не должно быть повторяющихся имен)
+  validates :name, presence: true, uniqueness: true
+
+  #has_secure_password заставляет Rails проверить совпадение двух паролей.
+  # Эта строка была добавлена потому,
+  # что при создании временной платформы вы указали password:digest.
+  # rails generate scaffold User name:string password:digest
   has_secure_password
+
 end
